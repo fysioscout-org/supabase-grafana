cee1f7368f11d5a71f301b2637a0f58a39e2b50a
Amir Khan <amir@adaptagency.com>
2024-05-07 12:47:39 +0200

    update entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,39 +19,38 @@ if [ -z "$SUPABASE_ACCESS_TOKEN" ]; then
 
   sed -i "s/__SUPABASE_PROJECT_REF__/$SUPABASE_PROJECT_REF/g" /etc/prometheus/prometheus.yml
   sed -i "s/__SUPABASE_SERVICE_ROLE_KEY__/$SUPABASE_SERVICE_ROLE_KEY/g" /etc/prometheus/prometheus.yml
+else
+  echo "Setting up multi-project monitoring."
+
+  FILTER='.[] | .id'
+  if [ -n "$SUPABASE_ORGANIZATION_ID" ]; then
+    FILTER=".[] | select(.organization_id == \"$SUPABASE_ORGANIZATION_ID\") | .id"
+  fi
+
+  PROJECT_REFS=$(curl -sX 'GET' 'https://api.supabase.com/v1/projects' \
+    -H 'accept: application/json' \
+    -H "Authorization: Bearer $SUPABASE_ACCESS_TOKEN" | jq -r "$FILTER")
+
+  for PROJECT_REF in $PROJECT_REFS; do
+    echo "Setting up project $PROJECT_REF"
+
+    API_KEYS_RESPONSE=$(curl -sX 'GET' "https://api.supabase.com/v1/projects/$PROJECT_REF/api-keys" \
+      -H 'accept: application/json' \
+      -H "Authorization: Bearer $SUPABASE_ACCESS_TOKEN")
+
+    if [[ $API_KEYS_RESPONSE == *"Failed to fetch project service details"* ]]; then
+      echo "Error: Could not retrieve API keys for project $PROJECT_REF. Project might be paused. Skipping."
+      continue
+    fi
+
+    SERVICE_ROLE_KEY=$(echo "$API_KEYS_RESPONSE" | jq -r '.[] | select(.name == "service_role") | .api_key')
+
+    cat /etc/prometheus/prometheus.target.yml.tpl >> /etc/prometheus/prometheus.yml
+
+    sed -i "s/__SUPABASE_PROJECT_REF__/$PROJECT_REF/g" /etc/prometheus/prometheus.yml
+    sed -i "s/__SUPABASE_SERVICE_ROLE_KEY__/$SERVICE_ROLE_KEY/g" /etc/prometheus/prometheus.yml
+  done
 fi
-# else
-#   echo "Setting up multi-project monitoring."
-
-#   FILTER='.[] | .id'
-#   if [ -n "$SUPABASE_ORGANIZATION_ID" ]; then
-#     FILTER=".[] | select(.organization_id == \"$SUPABASE_ORGANIZATION_ID\") | .id"
-#   fi
-
-#   PROJECT_REFS=$(curl -sX 'GET' 'https://api.supabase.com/v1/projects' \
-#     -H 'accept: application/json' \
-#     -H "Authorization: Bearer $SUPABASE_ACCESS_TOKEN" | jq -r "$FILTER")
-
-#   for PROJECT_REF in $PROJECT_REFS; do
-#     echo "Setting up project $PROJECT_REF"
-
-#     API_KEYS_RESPONSE=$(curl -sX 'GET' "https://api.supabase.com/v1/projects/$PROJECT_REF/api-keys" \
-#       -H 'accept: application/json' \
-#       -H "Authorization: Bearer $SUPABASE_ACCESS_TOKEN")
-
-#     if [[ $API_KEYS_RESPONSE == *"Failed to fetch project service details"* ]]; then
-#       echo "Error: Could not retrieve API keys for project $PROJECT_REF. Project might be paused. Skipping."
-#       continue
-#     fi
-
-#     SERVICE_ROLE_KEY=$(echo "$API_KEYS_RESPONSE" | jq -r '.[] | select(.name == "service_role") | .api_key')
-
-#     cat /etc/prometheus/prometheus.target.yml.tpl >> /etc/prometheus/prometheus.yml
-
-#     sed -i "s/__SUPABASE_PROJECT_REF__/$PROJECT_REF/g" /etc/prometheus/prometheus.yml
-#     sed -i "s/__SUPABASE_SERVICE_ROLE_KEY__/$SERVICE_ROLE_KEY/g" /etc/prometheus/prometheus.yml
-#   done
-# fi
 
 mkdir -p /data/grafana/data
 mkdir -p /data/grafana/plugins
